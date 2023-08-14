d979bb7c9ba5a41ef90e333b2a7e18bedfdd0a2a
ony19161 <aman.19161@gmail.com>
2023-08-14 22:30:22 +0600

    Added interfaces implementations and base files

---
+++ DbManagerSolution/DbManager/Interfaces/IDbContext.cs
@@ -0,0 +1,16 @@
+﻿using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Interfaces
+{
+    public interface IDbContext
+    {
+        Task<IDbConnection> CreateConnectionAsync();
+        QueryFactory GetDb(IDbConnection connection);
+    }
+}
