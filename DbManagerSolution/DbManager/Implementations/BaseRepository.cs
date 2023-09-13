efec82a8c3a54221a1dffa8590e714b436bf488d
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2023-09-13 23:18:07 +0600

    Added all the necessary comment to the baseRepository method

--- DbManagerSolution/DbManager/Implementations/BaseRepository.cs
+++ DbManagerSolution/DbManager/Implementations/BaseRepository.cs
@@ -24,12 +24,22 @@ namespace DbManager.Implementations
             _dbSet = context.Set<TEntity>();
         }
 
+        /// <summary>
+        ///  Deletes an entity from your respected database.
+        /// </summary>
+        /// <param name="entity">Your Entity(DB table) Class. ex: Student,Person etc</param>
+        /// <returns></returns>
         public async Task<int> DeleteAsync(TEntity entity)
         {
            _dbSet.Remove(entity);
            return await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Finds from your corresopnding Entity based on your provided predicate
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return await _dbSet.Where(predicate).ToListAsync();
@@ -40,25 +50,44 @@ namespace DbManager.Implementations
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get all data from the repected entity
+        /// </summary>
+        /// <returns></returns>
         public async Task<IList<TEntity>> GetAllAsync()
         {
             return await _dbSet.ToListAsync();
         }
 
+        /// <summary>
+        /// Takes an Id then return the entity object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<TEntity> GetByIdAsync(object id)
         {
             return await _dbSet.FindAsync(id);
         }
 
+        /// <summary>
+        /// Takes an entity object then inserts it into the table. (Entity)
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task<int> InsertAsync(TEntity entity)
         {
             _dbSet.Add(entity);
             return await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Takes an entity then updates it from the database table
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public Task<int> UpdateAsync(TEntity entity)
         {
-            throw new NotImplementedException();
         }
     }
 }
